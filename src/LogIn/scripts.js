ff932bd7085bbda34c10b07549988d25e8ff83ae
qinyuncao <qinyuncao@umass.edu>
2023-05-03 00:07:03 -0400

    Main, SignUp, LogIn 1st version

---
+++ src/LogIn/scripts.js
@@ -0,0 +1,13 @@
+function login() {
+    window.location.href = 'logIn.html';
+  }
+  
+  function signup() {
+    window.location.href = '../SignUp/signUp.html';
+  }
+  
+  function forgotPassword() {
+    // Navigate to the forgot password page or show the forgot password form
+    console.log('Forgot Password button clicked');
+  }
+  
---
+++ src/SignUp/scripts.js
@@ -0,0 +1,13 @@
+function login() {
+    window.location.href = '../LogIn/logIn.html';
+  }
+  
+  function signup() {
+    window.location.href = 'signUp.html';
+  }
+  
+  function forgotPassword() {
+    // Navigate to the forgot password page or show the forgot password form
+    console.log('Forgot Password button clicked');
+  }
+  
