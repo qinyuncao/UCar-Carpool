bc5389f14fe2382eda277404e23a93896551047f
peilan_wang <lw980627@gmail.com>
2023-05-10 09:31:02 -0400

    added backend with nodejs and mongodb web connection

---
+++ backend/main.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnviroment.mjs";
+import "express-async-errors";
+import route from "./route.mjs";
+
+const PORT = process.env.BACKEND_PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Load the /posts routes
+app.use("/route", route);
+
+// Global error handling
+app.use((err, _req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke! Check the back end!')
+})
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
