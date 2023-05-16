6e234e88466efb933bd31fdc77d2791dcde0dbfa
qinyuncao <qinyuncao@umass.edu>
2023-05-16 13:21:35 -0400

    Search update

--- src/Search/scripts.js
+++ src/Search/scripts.js
@@ -68,63 +68,50 @@ window.addEventListener('load', async () => {
     postContainer.appendChild(createPostCard(posts[i]));
   }
 
+  //   const response = await fetch('http://127.0.0.1:3000/ride/avaliable-rides',{
+  //     method:'GET',
+  //     headers: {
+  //         'Authorization':sessionStorage.getItem('token')
+  //     }
+  // });
+  //Use reponse instead after testing
+  userPosts = posts = [
+  {
+    "title": "Boston TO NY",
+    "date": "2019/5/5",
+    "time": "11:15 AM",
+    "departure": "Boston",
+    "destination": "Amherst",
+    "remainSlot": 4,
+    "phoneNum": "4133799999",
+    "details": "asldkfjeljflkajsdklfjalkwej"
+  },
+  {
+    "title": "Boston TO NY",
+    "date": "2019/5/5",
+    "time": "11:15 AM",
+    "departure": "Boston",
+    "destination": "Amherst",
+    "remainSlot": 4,
+    "phoneNum": "4133799999",
+    "details": "asldkfjeljflkajsdklfjalkwej"
+  }
+  ];
+
+  var userRideContainer = document.getElementById("userRide-container");
+  while (userRideContainer.firstChild) {
+    userRideContainer.removeChild(userRideContainer.firstChild);
+  }
+
+  for (var i = 0; i < userPosts.length; i++) {
+    userRideContainer.appendChild(createUserPostCard(userPosts[i]));
+  }
+
 
 
 
 
 });
-
-function createPostCard(post) {
-  let card = document.createElement('div');
-  card.className = 'card mb-2 mx-2';
-  card.style = 'width:30rem';
-
-  let img = document.createElement('img');
-  img.className = 'card-image-top'
-  img.src = '../photos/car.jpg'
-  // Create image based on the car model
-
-  let cardBody = document.createElement('div');
-  cardBody.className = 'card-body';
-
-  let title = document.createElement('h5');
-  title.innerText = post.title;
-  title.className = 'card-title';
-
-  let dateAndTime = document.createElement('h6');
-  dateAndTime.innerText = post.date.concat(" ", post.time);
-  dateAndTime.className = "card-subtitle mb-2 text-muted";
-
-  let departAndDest = document.createElement('div');
-  departAndDest.innerText = "Departure:" + post.departure + " | " + "Destination:" + post.destination;
-  departAndDest.className = 'card-text';
-
-  let remainSlot = document.createElement('div');
-  remainSlot.innerText = "Number of Passenger:" + post.remainSlot;
-  remainSlot.className = 'card-text';
-
-  let phoneNum = document.createElement('div');
-  phoneNum.innerText = "Contact Number: " + post.phoneNum;
-  phoneNum.className = 'card-text';
-
-  let details = document.createElement('div');
-  details.innerText = "More Information:" + post.details;
-  details.className = 'card-text';
-
-
-  cardBody.appendChild(title);
-  cardBody.appendChild(dateAndTime);
-  cardBody.appendChild(departAndDest);
-  cardBody.appendChild(remainSlot);
-  cardBody.appendChild(phoneNum);
-  cardBody.appendChild(details);
-
-
-
-  card.appendChild(img);
-  card.appendChild(cardBody);
-  return card
-}
 
 document.getElementById('searchbutton').addEventListener('click', async function () {
   const inputDestination = document.getElementById('inputDestination').value.toLowerCase();
@@ -201,3 +188,92 @@ document.getElementById('searchbutton').addEventListener('click', async function
 
 
 });
+
+function createPostCard(post) {
+  let card = document.createElement('div');
+  card.className = 'card mb-2 mx-2';
+  card.style = 'width:30rem';
+
+  let img = document.createElement('img');
+  img.className = 'card-image-top'
+  img.src = '../photos/car.jpg'
+  // Create image based on the car model
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h5');
+  title.innerText = post.title;
+  title.className = 'card-title';
+
+  let dateAndTime = document.createElement('h6');
+  dateAndTime.innerText = post.date.concat(" ", post.time);
+  dateAndTime.className = "card-subtitle mb-2 text-muted";
+
+  let departAndDest = document.createElement('div');
+  departAndDest.innerText = "Departure:" + post.departure + " | " + "Destination:" + post.destination;
+  departAndDest.className = 'card-text';
+
+  let remainSlot = document.createElement('div');
+  remainSlot.innerText = "Number of Passenger:" + post.remainSlot;
+  remainSlot.className = 'card-text';
+
+  let phoneNum = document.createElement('div');
+  phoneNum.innerText = "Contact Number: " + post.phoneNum;
+  phoneNum.className = 'card-text';
+
+  let details = document.createElement('div');
+  details.innerText = "More Information:" + post.details;
+  details.className = 'card-text';
+
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(dateAndTime);
+  cardBody.appendChild(departAndDest);
+  cardBody.appendChild(remainSlot);
+  cardBody.appendChild(phoneNum);
+  cardBody.appendChild(details);
+
+
+
+  card.appendChild(img);
+  card.appendChild(cardBody);
+  return card
+}
+
+function createUserPostCard(post) {
+  let card = document.createElement('div');
+  card.className = 'card mb-2';
+  card.style = 'width:31rem';
+
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  let title = document.createElement('h6');
+  title.innerText = post.title;
+  title.className = 'card-title';
+
+  let dateAndTime = document.createElement('div');
+  dateAndTime.innerText = post.date.concat(" ", post.time);
+  dateAndTime.className = "card-subtitle mb-2 text-muted";
+
+  let departAndDest = document.createElement('div');
+  departAndDest.innerText = "Departure:" + post.departure + " | " + "Destination:" + post.destination;
+  departAndDest.className = 'card-text';
+
+  let deleteButton = document.createElement('input');
+  deleteButton.value = 'delete';
+  deleteButton.type = 'button';
+  deleteButton.className = 'btn btn-danger text-white btn-sm';
+  
+
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(dateAndTime);
+  cardBody.appendChild(departAndDest);
+  cardBody.appendChild(deleteButton);
+
+  card.appendChild(cardBody);
+  return card
+}
